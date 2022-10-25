a1c2c7aef60ecf04d7827274f68749ed0e16b0c5
Julker Nien Akib <julkernienakib@gmail.com>
2022-10-25 23:44:22 +0600

    first commit

---
+++ database/migrations/2022_10_25_172741_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->nullable();
+            $table->Integer('cat_id')->unsigned()->nullable();
+            $table->Integer('product_type')->unsigned()->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+            $table->foreign('cat_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
